4595364c95b5e41a295691a3b38fe086cf4c59d7
Andreas Kissner <andikiss500@gmail.com>
2025-07-02 08:17:18 +0200

    Added render big card js asynx

--- scripts_folder/render.js
+++ scripts_folder/render.js
@@ -13,7 +13,25 @@ function renderMiniCard(start = 0) {
 
   for (let i = start; i < allPokemons.length; i++) {
     const miniCard = getMiniCardTemplate(allPokemons[i]);
-    miniCardContent.insertAdjacentHTML("beforeend", miniCard);
+    miniCardContent.insertAdjacentHTML("beforeend", miniCard);//Method that allows you to insert HTML code as a string directly into the DOM – without creating a new DOM element with createElement
   }
   colorMiniCards();
 }
+
+
+
+async function renderBigCard(pokemon) {
+  const overlay = document.getElementById("big-card-overlay");
+  const weaknessText = await loadWeakness(pokemon);
+  const evoList = await loadEvolution(pokemon);
+
+  const evo1 = evoList[0] || { name: "?", img: "./assets/image/placeholder.png" }; // EVO IMAGE
+  const evo2 = evoList[1] || { name: "?", img: "./assets/image/placeholder.png" };
+  const evo3 = evoList[2] || { name: "?", img: "./assets/image/placeholder.png" };
+
+  overlay.innerHTML = getBigCardTemplate(pokemon, weaknessText, evo1, evo2, evo3);
+  overlay.classList.remove("d-none");
+  document.body.style.overflow = "hidden";
+  colorBigCard(pokemon);
+}
+
