37bb366ec3443cee7434840b64c1bdbef85cd5e9
Andreas Kissner <andikiss500@gmail.com>
2025-07-02 08:14:33 +0200

    Added render mini card js

--- scripts_folder/render.js
+++ scripts_folder/render.js
@@ -7,3 +7,13 @@ function renderFooter(){
     let footerContent = document.getElementById("footer");
     footerContent.innerHTML = getFooterTemplate();
 }
+
+function renderMiniCard(start = 0) {
+  const miniCardContent = document.getElementById("mini-card-content");
+
+  for (let i = start; i < allPokemons.length; i++) {
+    const miniCard = getMiniCardTemplate(allPokemons[i]);
+    miniCardContent.insertAdjacentHTML("beforeend", miniCard);
+  }
+  colorMiniCards();
+}
