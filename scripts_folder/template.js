bacc67f4719141cfd79865af0e51846272fca2b3
Andreas Kissner <andikiss500@gmail.com>
2025-07-02 08:24:13 +0200

    Added template for footer

--- scripts_folder/template.js
+++ scripts_folder/template.js
@@ -9,3 +9,22 @@ function getHeaderTemplate(){
     </header>
     `;
 }
+
+
+
+function getFooterTemplate(){
+    return`
+      <footer class="bg-dark text-light py-4 ">
+        <div class="footer-container text-center">
+            <p class="mb-1">Thanks for exploring the world of Pokémon!</p>
+            <p class="mb-2">Created with the FORCE by Andreas – for all trainers out there.</p>
+            <a href="https://www.pokemon.com/us/" target="_blank" class="text-warning text-decoration-none">
+                Visit the official Pokémon website
+            </a>
+            <div class="mt-3">
+                <img src="./assets/image/spinner.svg" alt="Pokéball" width="40" height="40">
+            </div>
+        </div>
+    </footer>
+    `;
+}
